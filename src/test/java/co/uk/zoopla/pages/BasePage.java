08962929f9549a9d2fb9aa75d788fda8cec5112e
Ola <olawunmi263@yahoo.com>
2020-05-26 04:39:42 +0100

    URL Successfully launched

--- src/test/java/co/uk/zoopla/pages/BasePage.java
+++ src/test/java/co/uk/zoopla/pages/BasePage.java
@@ -1,11 +1,20 @@
 package co.uk.zoopla.pages;
 
 import co.uk.zoopla.commons.DriverLib;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class BasePage extends DriverLib {
     private String BASE_URL = "https://www.zoopla.co.uk/";
+    public Select select;
 
     public void launchURL(){
         driver.navigate().to(BASE_URL);
     }
+
+    public void selectElementByText(WebElement element, String text){
+        select = new Select(element);
+        select.selectByVisibleText(text);
+    }
+
 }
