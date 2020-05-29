8353ef6890ba252715248b02c6cbf61673210dda
Ola <olawunmi263@yahoo.com>
2020-05-29 05:28:40 +0100

    URL Successfully launched

--- src/test/java/co/uk/zoopla/pages/BasePage.java
+++ src/test/java/co/uk/zoopla/pages/BasePage.java
@@ -2,13 +2,17 @@ package co.uk.zoopla.pages;
 
 import co.uk.zoopla.commons.DriverLib;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage extends DriverLib {
-    private String BASE_URL = "https://www.zoopla.co.uk/";
+
     public Select select;
 
     public void launchURL(){
+        String BASE_URL = "https://www.zoopla.co.uk/";
         driver.navigate().to(BASE_URL);
     }
 
@@ -16,5 +20,18 @@ public class BasePage extends DriverLib {
         select = new Select(element);
         select.selectByVisibleText(text);
     }
+
+    public  void selectElementByValue(WebElement element, String value){
+        select  = new Select(element);
+        select.selectByValue(value);
+    }
+    public  void selectElementByIndex(WebElement element, int index){
+        select  = new Select(element);
+        select.selectByIndex(index);
+    }
+public void waitForElementToBeDisplayed(WebElement element){
+    wait = new WebDriverWait(driver, 10);
+    wait.until(ExpectedConditions.visibilityOf(element));
+}
 
 }
